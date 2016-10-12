309aa0ac735c4cf9dc983369b27c792ee421c6b1
fredpiuma <fredpiuma@gmail.com>
2016-10-12 15:44:23 +0000

    melhorias

--- asana-hacks-gmail.user.js
+++ asana-hacks-gmail.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Asana Haks for Gmail
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  
 // @author       You
 // @match        https://mail.google.com/mail/u/*
@@ -10,6 +10,7 @@
 
 (function() {
 	'use strict';
+	
 	setInterval(function() {
 
 		/* remove signature on asana reply */
@@ -17,13 +18,14 @@
 		if($from.length > 0) {
 			if( /reply-.+@asana.com/.test( $from[0].getAttribute('email') ) ) {
 				var $dots = document.querySelectorAll('.aH1');
-				if( $dots.length > 0 && !$from[0].getAttribute('removed') ) {
-					$from[0].setAttribute('removed', true);
+				if( $dots.length > 0 ) {
 					$dots[0].parentNode.click();
+					$dots[0].parentNode.remove();
 					document.getElementsByClassName('editable')[0].innerHTML = '';
 				}
 			}
 		}
 		
 	},500);
+
 })();
